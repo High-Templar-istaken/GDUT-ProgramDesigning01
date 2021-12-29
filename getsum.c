79d7134d7de413eb675b396caa539e13214edda2
high_templar <Gene_Work@163.com>
2021-12-30 00:11:15 +0800

    Add more utility function in the 'sum' operation.

--- getsum.c
+++ getsum.c
@@ -1,18 +1,9 @@
 #include "mycheck.h"
 
-int getsum(int argc,char *argv[])
+void getsum_add_mode(char *header)
 {
 	double sum=0;
-	
-	ReadTable("./.mycheck/working.txt");
-	if(argc > 3 || argc <= 2)
-	{
-		printf("ERROR: In function 'getsum': Not enough arguments!\n");
-		exit(0);
-	}
-	
 	PKEY nowkey = NULL;
-	char *header = argv[2];
 	
 	if(QueryHeaderID(header) == NULL)
 	{
@@ -38,5 +29,89 @@ int getsum(int argc,char *argv[])
 	
 	if(ExitMute == false) printf("The result of summing up header '%s' is %lf\n",header,sum);
 	else printf("%lf",sum);
+	
+	return;
+}
+
+void getsum_numrow_mode()
+{
+	int cnt=0;
+	
+	PROW nowrow = rowbegin;
+	
+	while(nowrow != NULL)
+	{
+		++cnt;
+		nowrow = (*nowrow).nxtrow;
+	}
+	
+	if(ExitMute == false) printf("rows remain: %d\n",cnt);
+	else printf("%d",cnt);
+	
+	return;
+}
+
+void getsum_numheader_mode()
+{
+	int cnt=0;
+	
+	PHEADER nowheader = headerbegin;
+	
+	while(nowheader != NULL)
+	{
+		++cnt;
+		nowheader = (*nowheader).nxtheader;
+	}
+	
+	if(ExitMute == false) printf("headers remain: %d\n",cnt);
+	else printf("%d",cnt);
+	
+	return;
+}
+
+int getsum(int argc,char *argv[])
+{
+	ReadTable("./.mycheck/working.txt");
+	
+	if(argv[2] == NULL)
+	{
+		printf("ERROR: In function 'getsum': Not enough arguments!\n");
+		exit(0);
+	}
+	
+	if(strcmp(argv[2],"-add") == 0)
+	{
+		if(argv[3] == NULL)
+		{
+			printf("ERROR: Not enough arguments in the '-add'\n");
+			exit(0);
+		}
+		getsum_add_mode(argv[3]);
+	}
+	else if(strcmp(argv[2],"-num=row") == 0)
+	{
+		if(argv[3] != NULL)
+		{
+			printf("ERROR: Too many arguments in the '-num'\n");
+			exit(0);
+		}
+		getsum_numrow_mode();
+	}
+	else if(strcmp(argv[2],"-num=header") == 0)
+	{
+		if(argv[3] != NULL)
+		{
+			printf("ERROR: Too many arguments in the '-num'\n");
+			exit(0);
+		}
+		getsum_numheader_mode();
+	}
+	else
+	{
+		printf("ERROR: Unrecognized argument '%s'\n",argv[2]);
+		exit(0);
+	}
+	
+	
 	return 0;
 }
